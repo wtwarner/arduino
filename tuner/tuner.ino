cac0f06ab9505ffe18d66d1d3a3c8db99d8fc2a2
Bill Warner <wwarnerjr@apple.com>
2021-02-21 10:02:11 -0800

    add new AudioTuner library

--- tuner/tuner.ino
+++ tuner/tuner.ino
@@ -5,6 +5,7 @@
 #include "global.h"
 #include "Yin.h"
 #include "Yin16.h"
+#include <AudioTuner.h>
 
 const int DECIMATE_FACTOR = 4;
 const int NUM_BUFFERS = 18;
@@ -13,16 +14,26 @@ const int NUM_BUFFERS = 18;
 AudioInputAnalog         adc1(A2);           //xy=153,113
 AudioFilterBiquad        biquad1;
 //AudioAnalyzeNoteFrequency notefreq1;      //xy=351,110
+AudioTuner               notefreq1;
 AudioRecordQueue         queue1;         //xy=332,215
 //AudioConnection          patchCord1(adc1, notefreq1);
 AudioConnection          patchCord3(adc1, biquad1);
 AudioConnection          patchCord2(biquad1, queue1);
-//AudioConnection           patchCord4(adc1, notefreq1);
+AudioConnection           patchCord4(biquad1, notefreq1);
 // GUItool: end automatically generated code
 
 Yin yin(AUDIO_SAMPLE_RATE_EXACT / (float)DECIMATE_FACTOR, 128 * NUM_BUFFERS / DECIMATE_FACTOR);
 
 Yin16 yin16;
+
+// For AudioTuner, use decimation_Factor 4
+int16_t fir_11029_HZ_coefficients[22] =
+{
+    0, 3, 6, -11, -71, 21,
+    352, -15, -1202, -6, 5011, 8209,
+    5011, -6, -1202, -15, 352, 21,
+    -71, -11, 6, 3
+};
 
 void setup() {
   AudioMemory(30);
@@ -34,7 +45,7 @@ void setup() {
   }
   Serial.println("start..");
   // put your setup code here, to run once:
-  //notefreq1.begin(.45);
+  notefreq1.begin(.15, fir_11029_HZ_coefficients, sizeof(fir_11029_HZ_coefficients), DECIMATE_FACTOR);
   
   digitalWriteFast(LED_BUILTIN, 1);
 
@@ -88,16 +99,15 @@ float autocor(int16_t *rawData, int len) {
 int16_t mem[128 * NUM_BUFFERS / DECIMATE_FACTOR];
 float mem_fp[128 * NUM_BUFFERS / DECIMATE_FACTOR];
 
-#if 0
+#if 1
 void loop()
 {
     while (!notefreq1.available())
-   ;
-  Serial.printf("NF %03.2f\n", notefreq1.read(), notefreq1.probability());
-  //notefreq1.end();
+        ;
+    Serial.printf("NF %03.2f\n", notefreq1.read(), notefreq1.probability());
 }
 #endif  
-void loop() {
+void loop1() {
   // put your main code here, to run repeatedly:
   //delay(5000);
 
