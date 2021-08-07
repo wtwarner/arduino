d818d93415ef07a6969960fd8098c83d4d6b3337
Bill Warner <wtwarner@gmail.com>
2021-08-07 14:12:33 -0700

    add rtc; move to Pro Micro board

--- iv25-clock/iv25-clock.ino
+++ iv25-clock/iv25-clock.ino
@@ -1,13 +1,35 @@
-static const int PIN_CLK = 12;
-static const int PIN_SDI = 11;
-static const int PIN_STROBE = 10;
+#include <TimeLib.h>
+#include <Timezone_Generic.h>
+
+//static const int PIN_CLK = 12;
+//static const int PIN_SDI = 11;
+//static const int PIN_STROBE = 10;
+//static const int PIN_OE_ = 9;
+//static const int PIN_LED = LED_BUILTIN;
+
+static const int PIN_CLK = 6;
+static const int PIN_SDI = 7;
+static const int PIN_STROBE = 8;
 static const int PIN_OE_ = 9;
+static const int PIN_LED = 17;  // TX
+
+extern bool ds3231_setup(int clockSqwPin, void (*isr)(void));
+
+//
+// TimeZone
+//
+TimeChangeRule myPDT = {"PDT", Second, Sun, Mar,
+                        2,     -60 * 7}; // Daylight time = UTC - 7 hours
+TimeChangeRule myPST = {"PST", First, Sun,
+                        Nov,   2,     -60 * 8}; // Standard time = UTC - 8 hours
+Timezone myTZ(myPDT, myPST);
+TimeChangeRule *tcr; // pointer to the time change rule, use to get TZ abbrev
 
 void setup() {
   Serial.begin(9600);
   delay(1500);
   Serial.println("Start");
- pinMode(LED_BUILTIN, OUTPUT);
+ pinMode(PIN_LED, OUTPUT);
 
  pinMode(PIN_CLK, OUTPUT);
  pinMode(PIN_SDI, OUTPUT);
@@ -17,7 +39,24 @@ void setup() {
  digitalWrite(PIN_SDI, 0);
  digitalWrite(PIN_STROBE, 0);
  digitalWrite(PIN_OE_, 1);
- 
+
+  if (ds3231_setup()) {
+     Serial.println("ds3231_setup succeeded");
+     time_t utc = ds3231_get_unixtime();
+     time_t local = myTZ.toLocal(utc, &tcr);
+     Serial.println("Local time:");
+     printDateTime(local, tcr->abbrev);
+  }
+  else {
+    Serial.println("ds3231_setup failed; setting");
+   // local "ISO 8601" format e.g. 2020-06-25T15:29:37
+      time_t local_time = datetime_parse("2021-08-07T14:02:00");
+      time_t new_utc = myTZ.toUTC(local_time);
+       Serial.print("Set time: ");
+        printDateTime(new_utc, tcr->abbrev);
+  
+    ds3231_set(new_utc);
+  }
 }
 
 const uint8_t map_seg_to_bit[7] = {
@@ -29,10 +68,7 @@ void loop() {
    uint16_t seg_bit0 = ((cnt/7) == 0) ? (1 << map_seg_to_bit[cnt % 7]) : 0;
    uint16_t seg_bit1 = ((cnt/7) == 1) ? (1 << map_seg_to_bit[cnt % 7]) : 0;
    uint16_t sdi_d = seg_bit0 | (seg_bit1 << 8);
-   //uint16_t sdi_d = (1 << map_seg_to_bit[cnt % 7]) << 8;
-   Serial.print(cnt); Serial.print(" ");
-   Serial.print(sdi_d, HEX); Serial.print("\n");
-   for (int i = 0; i < 16; i ++) {
+     for (int i = 0; i < 16; i ++) {
       digitalWrite(PIN_SDI, (sdi_d >> (15 - i)) & 0x1);
       digitalWrite(PIN_CLK, 1);
       digitalWrite(PIN_CLK, 0);
@@ -44,4 +80,16 @@ void loop() {
    
    delay(250);
    cnt=(cnt+1)%14;
+   digitalWrite(PIN_LED, cnt&1);
+}
+
+void printDateTime(time_t t, const char *tz) {
+  char buf[32];
+
+  char m[4]; // temporary storage for month string (DateStrings.cpp uses shared
+             // buffer)
+  strcpy(m, monthShortStr(month(t)));
+  sprintf(buf, "%.2d:%.2d:%.2d %s %.2d %s %d %s", hour(t), minute(t), second(t),
+          dayShortStr(weekday(t)), day(t), m, year(t), tz);
+  Serial.println(buf);
 }
