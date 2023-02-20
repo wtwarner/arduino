cea7c7ffd77d256fc78ebdac6e97e425075da6d9
Bill Warner <wtwarner@gmail.com>
2023-02-19 19:24:12 -0800

    update voltage reference

--- voltageReference/voltageReference.ino
+++ voltageReference/voltageReference.ino
@@ -10,34 +10,38 @@
  * 
  */
  #include "PID_v1.h"
- #include "PID_AutoTune_v0.h"
  #include "elapsedMillis.h"
+
+ const int PIN_REACHTEMP_LED = A1;
+ const int PIN_TEMP = A7; // MCP9701 output
+ const int PIN_HEATER_PWM = 3; // other LED is on this (after AND with Watchdog)
+ const int PIN_HEATER_WATCHDOG = 4; // pulse to 555 reset
  
- double pid_setpoint, pid_input, pid_output=128;
- PID heaterPID(&pid_input, &pid_output, &pid_setpoint, 3, 1, .2, DIRECT);
- PID_ATune heaterPIDAtune(&pid_input, &pid_output);
+ double pid_setpoint, pid_input, pid_output=0;
+ PID heaterPID(&pid_input, &pid_output, &pid_setpoint, 2, .8, .2, DIRECT);
  
  void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  Serial.begin(38400);
+
   analogReference(INTERNAL); // 1.1V
-  pinMode(A7, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  digitalWrite(4, 1);
-  digitalWrite(3, 0);
+  pinMode(PIN_TEMP, INPUT);
   
-  pid_input = 10.0; // degrees C
+  pinMode(PIN_HEATER_PWM, OUTPUT);
+  digitalWrite(PIN_HEATER_PWM, 0);
+  
+  pinMode(PIN_HEATER_WATCHDOG, OUTPUT);
+  digitalWrite(PIN_HEATER_WATCHDOG, 1);
+  
+  pinMode(PIN_REACHTEMP_LED, OUTPUT);
+  digitalWrite(PIN_REACHTEMP_LED, 0);
+  
+  pid_input = 25.0; // degrees C
   pid_setpoint = 30.0; // degrees C
   heaterPID.SetSampleTime(10);
   heaterPID.SetMode(AUTOMATIC);
 }
 
 int cnt = 0;
-bool tuned = false;
-elapsedMillis startTuning;
 elapsedMillis watchdog;
 /*
  * (((744/1024*1.1*65536) - .4*65536)/.0196)/65536
@@ -49,12 +53,11 @@ int32_t read_mcp9701()
 {
   int32_t raw_v;
   for (int i = 0; i < 4; i++)
-    raw_v = analogRead(A7);
-  //float v = raw_v/1024.0*1.1;
-  //float deg_c = (v - .400) / .0196; // MCP9701 
+    raw_v = analogRead(PIN_TEMP);
+
   int32_t deg_c = (raw_v) * 3592 - 1337469;
- //Serial.print(raw_v);Serial.print(" "); Serial.println( deg_c); delay(100);
-   return deg_c;
+  //Serial.print(raw_v);Serial.print(" "); Serial.println( deg_c); delay(100);
+  return deg_c;
 }
 
 /*
@@ -76,62 +79,55 @@ int32_t read_inttemp()
      return c;
 }
 
+
 void loop() {
   long startUs = micros();
-  digitalWrite(3, 0);
+
+  // quiese
   delayMicroseconds(10);
  
-  #if 1
-   int32_t deg_c = read_mcp9701(); 
-  #else
-  int32_t deg_c = 25*65536;
-  #endif
+  int32_t deg_c = read_mcp9701(); 
   
-  pid_input = deg_c;
-  #if 1
+  pid_input = (float)deg_c/65536.0;
   int32_t inttemp_c = read_inttemp();
-  #else
-  int32_t inttemp_c = 25;
-  #endif
-  //heaterPID.Compute();
-  //float deg_f = deg_c*9.0/5.0 + 32.0;
-  
-  
-//    if (!tuned && startTuning > 10000) {
-//       
-//      if (heaterPIDAtune.Runtime())
-//      {
-//        tuned = true;
-//        //we're done, set the tuning parameters
-//        float kp = heaterPIDAtune.GetKp();
-//        float ki = heaterPIDAtune.GetKi();
-//        float kd = heaterPIDAtune.GetKd();
-//        //heaterPID.SetTunings(kp,ki,kd);
-//        
-//          Serial.println("tuned:");
-//          Serial.println(kp); Serial.println(ki); Serial.println(kd);
-//      }
-//    }
-        
-    //analogWrite(3, (int)pid_output);
+
+  heaterPID.Compute();
+
+  // do manual pulse width modulation
   delayMicroseconds(255-(int)pid_output);
-  if (inttemp_c < 3276800) // 50c in s16.16
-    digitalWrite(3, 1);
-  delayMicroseconds((int)pid_output);
+
+  // turn on, but only if internal temp is safe
+  if ((int)pid_output > 0 && inttemp_c < 3276800) { // 50c in s16.16
+    digitalWrite(PIN_HEATER_PWM, 1);
+    delayMicroseconds((int)pid_output);
+  }
+  // turn off
+  digitalWrite(PIN_HEATER_PWM, 0);
+
+  // LED: open-drain, drive low to turn on.  Tristate to turn off.
+  // On if temp is between 29.5 and 30.5 (in s16.16)
+  const bool temp_led = (deg_c >= 1933312) && (deg_c <= 1998848);
+  if (temp_led) {
+    pinMode(PIN_REACHTEMP_LED, OUTPUT);
+    digitalWrite(PIN_REACHTEMP_LED, 0);
+  }
+  else {
+    pinMode(PIN_REACHTEMP_LED, INPUT);
+  }
 
   if (++cnt == 250) {
-    
      Serial.print(pid_output); Serial.print(" "); Serial.print(" "); Serial.print(deg_c); Serial.print(" "); Serial.print(deg_c/65536.0);Serial.print(" "); Serial.println(inttemp_c/65536.0); 
      cnt = 0;
-
   }
   
-  // watchdog
+  // watchdog reset pulse to 555 timer
   if (watchdog > 500) {
-    digitalWrite(4, 0);
+    digitalWrite(PIN_HEATER_WATCHDOG, 0);
   }
   if (watchdog > 502) {
-    digitalWrite(4, 1);
+    digitalWrite(PIN_HEATER_WATCHDOG, 1);
     watchdog = 0;
   }
+    
+  
 }
