8ea374475f63a03b157c5a930637aa0651865475
Bill Warner <wtwarner@gmail.com>
2025-11-08 21:27:50 -0800

    update

--- boost_150v/code.py
+++ boost_150v/code.py
@@ -5,7 +5,16 @@
 import board
 import pwmio
 import time
-from digitalio import DigitalInOut, Direction, Pull
+import usb_cdc
+import analogio
+import digitalio
+
+pin_hv = analogio.AnalogIn(board.A5)
+# 34v: 7329
+# 62v: 12995
+# 80v: 16548
+# 105v: 20757
+# 140v: 27814
 
 if hasattr(board, "APA102_SCK"):
     import adafruit_dotstar
@@ -13,27 +22,67 @@ if hasattr(board, "APA102_SCK"):
 else:
     import neopixel
     rgbled = neopixel.NeoPixel(board.NEOPIXEL, 1)
-rgbled.brightness = 0.5
+rgbled.brightness = 0.25
 
 # Built in red LED
-led = DigitalInOut(board.D13)
-led.direction = Direction.OUTPUT
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
 
 
 # PWM
-pwm = pwmio.PWMOut(board.D5, frequency=42000)
-pwm.duty_cycle = int(.5 * 65535)
+freq = 30000
+duty = .5
+pwm = pwmio.PWMOut(board.D5, frequency=freq, variable_frequency=True)
+pwm.frequency = int(freq)
+pwm.duty_cycle = int(duty * 65535)
 
 ######################### MAIN LOOP ##############################
 
 led.value = 1
-delta = -0.001
+delta = -0.002
+print("start")
+line=""
+ramp=True
 while True:
   time.sleep(.025)
   led.value = not led.value
-  if (pwm.duty_cycle < .05*65535 and delta < 0) or (pwm.duty_cycle > .60*65535 and delta > 0):
+  if (pwm.duty_cycle < .05*65535 and delta < 0) or (pwm.duty_cycle > .50*65535 and delta > 0):
       delta = delta * -1
-  pwm.duty_cycle = pwm.duty_cycle + int(delta * 65535)
+  if ramp:
+      duty = duty + delta
+
+  hv_sum = 0
+  for _ in range(6):
+      hv_sum += pin_hv.value
+      time.sleep(.001)
+  # emergency stop
+  v=int(.005 * hv_sum/6.0)
+  #print((v,))
+  if v > 190:
+      duty = .1
+      print("STOP!")
+
+  pwm.duty_cycle = int(duty * 65535)
+
   rgbled[0] = (int(255 * pwm.duty_cycle/65535.0), 10, int(255 * (65535-pwm.duty_cycle)/65535.0))
 
+  if usb_cdc.console.in_waiting:
+    c = usb_cdc.console.read(1).decode('utf-8')
+    line = line + c
+    if c == '\n':
+        if line[0] == 'f':
+            freq = int(line[1:])
+            print("freq ", freq)
+            pwm.frequency = freq
+        elif line[0] == 'd':
+            duty = float(line[1:])/100.0
+            print("duty ", duty)
+        elif line[0] == 'r':
+            ramp = not ramp
+        elif line[0] == 'v':
+            v=pin_hv.value
+            print("v:", v, int(.005*v),"V")
+        line=""
+    elif c:
+        print("["+line+"]..")
 
--- vfd-snowflake/vfd-snowflake.ino
+++ vfd-snowflake/vfd-snowflake.ino
@@ -4,16 +4,28 @@
 
 #include "TimerHelpers.h"
 
-const int PAD_SD=3, PAD_SCLK=2, PAD_RCLK=4;
+const int PAD_SD1=3, PAD_SCLK=2, PAD_RCLK=4, PAD_SD2=5;
 const int PAD_FIL0=9, PAD_FIL1=10;  // filament "A/C" waveform
+
+enum { SEG_L, SEG_M, SEG_H};
+static const byte tube_shift[4] = {13, 10, 5, 2};
+
+static const byte vfd_seg_map[4][3] = { // seg index to bit mask
+  { 4,1,2 },
+  { 4,1,2 },
+  { 4,1,2 },
+  { 4,2,1 },  // BUG
+};
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PAD_SD, OUTPUT);
+  pinMode(PAD_SD1, OUTPUT);
+  pinMode(PAD_SD2, OUTPUT);
   pinMode(PAD_SCLK, OUTPUT);
   pinMode(PAD_RCLK, OUTPUT);
   pinMode(PAD_FIL0, OUTPUT);
   pinMode(PAD_FIL1, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   // set up Timer 0
   TCNT0 = 0;
@@ -27,7 +39,8 @@ void setup() {
 void shiftOut(uint16_t d)
 {
   for (int i = 0; i < 16; i ++) {
-    digitalWrite(PAD_SD, (d>>i) & 1);
+    digitalWrite(PAD_SD1, (d>>i) & 1);
+    digitalWrite(PAD_SD2, (d>>i) & 1);
     digitalWrite(PAD_SCLK, 1);
     digitalWrite(PAD_SCLK, 0);
   }
@@ -36,8 +49,23 @@ void shiftOut(uint16_t d)
 }
 void loop() {
   // put your main code here, to run repeatedly:
-   shiftOut(0xffff);
-   delay(500);
+  #if 1
+  const int D = 100;
+  for (int t = 0; t < 4; t++) {
+   digitalWrite(LED_BUILTIN, t&1);
+   shiftOut(vfd_seg_map[t][SEG_L] << tube_shift[t] );
+   delay(D);
+   shiftOut((vfd_seg_map[t][SEG_L]|vfd_seg_map[t][SEG_M]) << tube_shift[t]);
+   delay(D);
+   shiftOut((vfd_seg_map[t][SEG_L]|vfd_seg_map[t][SEG_M]|vfd_seg_map[t][SEG_H]) << tube_shift[t]);
+   delay(D);
+   shiftOut((vfd_seg_map[t][SEG_M]|vfd_seg_map[t][SEG_H]) << tube_shift[t]);
+   delay(D);
+   shiftOut(vfd_seg_map[t][SEG_H] << tube_shift[t]);
+   delay(D);
+  }
+   #endif
+
    shiftOut(0x0000);
-   delay(500);
+   delay(D);
 }
